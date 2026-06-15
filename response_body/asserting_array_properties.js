7be175aea32871b466252a3449b41b75a90ddbe5
ansh <anshshah2508@gmail.com>
2026-06-15 15:28:23 +0530

    Update asserting_array_properties.js

--- response_body/asserting_array_properties.js
+++ response_body/asserting_array_properties.js
@@ -1,1 +1,20 @@
+//check if any array is empty and if it contains perticular items:
+/*THE EXAMPLE STRUCTURE:
+{
+"id":1,
+"name":"john",
+"phone":"1234567890",
+"courses":["java", "selenium"]
+}
+*/
 
+//THE TESTING CODE:
+pm.test("Test array properties", () =>
+  {
+    //courses include "java"
+    pm.expect("jsonData.courses").to.include("java");
+
+    //courses array must include all listed
+    pm.expect(jsonData.courses).to.have.members(["java", "selenium"]);
+  }  
+);
