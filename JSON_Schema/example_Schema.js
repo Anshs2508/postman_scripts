eb734c3d784134ed4e02d9a03d4096166fa16195
ansh <anshshah2508@gmail.com>
2026-06-16 11:30:49 +0530

    Update example_Schema.js

--- JSON_Schema/example_Schema.js
+++ JSON_Schema/example_Schema.js
@@ -1,1 +1,48 @@
+//HERE'S THE BASIC EXAMPLE OF JSON SCHEMA:
+/*The Response:
+{
+  "id": 1,
+  "name": "john",
+  "phone": "1234567890",
+  "courses": ["java", "selenium"]
+}*/
+
+/*THE DATA TYPES:
+| Field    | Type   |
+| -------- | ------ |
+| id       | Number |
+| name     | String |
+| phone    | String |
+| courses  | Array  |
+| java     | String |
+| selenium | String |
+*/
+
+//JSON SCHEMA FOR THIS EXAMPLE::
+{
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "number"
+    },
+    "name": {
+      "type": "string"
+    },
+    "phone": {
+      "type": "string"
+    },
+    "courses": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    }
+  },
+  "required": [
+    "id",
+    "name",
+    "phone",
+    "courses"
+  ]
+}
 
